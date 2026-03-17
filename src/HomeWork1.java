7b0e37368fe2c9887799a46c455d2717a66274ca
Alex <borzetsov@gmail.com>
2026-03-17 16:09:05 +0700

    HomeWork 1 Initial commit

---
+++ src/HomeWork1.java
@@ -0,0 +1,11 @@
+/**
+ * Домашнее задание №1
+ * @author Aleksei Borzetsov
+ * 26.02.2026
+ */
+
+public class HomeWork1 {
+    public static void main(String[] args){
+        System.out.println("Initial commit");
+    }
+}
