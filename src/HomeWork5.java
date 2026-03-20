de14ede90a75c8294e516e89ffe3a8e81e530a5e
Alex <borzetsov@gmail.com>
2026-03-20 13:23:52 +0700

    HomeWork5 Initial commit

---
+++ src/HomeWork5.java
@@ -0,0 +1,11 @@
+/**
+ * Домашнее задание №5
+ * @author Aleksei Borzetsov
+ * 03/15/26
+ */
+
+public class HomeWork5 {
+    public void run(){
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@
 
 public class Main {
     public static void main(String[] args){
-        HomeWork4 hw4 = new HomeWork4();
-        hw4.run();
+        HomeWork5 hw5 = new HomeWork5();
+        hw5.run();
     }
 }
