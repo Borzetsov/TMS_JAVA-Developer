61df8b54735b9a5332adf64637788ce81712f570
Aleksei Borzetsov <borzetsov@gmail.com>
2026-03-22 17:21:23 +0700

    HomeWork7 Initial commit

---
+++ src/HomeWork7/HomeWork7.java
@@ -0,0 +1,20 @@
+/**
+ * Classname    HomeWork7
+ * @version     0.01
+ * @author      Aleksei Borzetsov
+ * date         22.03.2026
+ */
+ 
+package HomeWork7;
+
+import java.awt.geom.Point2D;
+
+public class HomeWork7 {
+
+    public void run() {
+
+        /*Шапка отчета*/
+        System.out.println("; Алексей Борзецов | Домашняя работа №7 | Выдано 18.03.2026 ;");
+        System.out.println();
+    }
+}
