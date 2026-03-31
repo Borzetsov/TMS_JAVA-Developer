191a4b2bfedbf36e4e8c5e420ed61429e7dc7bd5
Alex <borzetsov@gmail.com>
2026-03-31 13:24:08 +0700

    HomeWork10 Initial commit

---
+++ src/HomeWork10/HomeWork10.java
@@ -0,0 +1,17 @@
+/**
+ * Classname    HomeWork10
+ * @version     0.01
+ * @author      Aleksei Borzetsov
+ * date         31.03.2026
+ */
+ 
+package HomeWork10;
+
+public class HomeWork10 {
+
+    public void run() {
+        /*Шапка отчета*/
+        System.out.println("; Алексей Борзецов | Домашняя работа №10 | Выдано 30.03.2026 ;");
+        System.out.println();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 /**
  * Classname    Main
- * @version     0.09
+ * @version     0.10
  * @author      Aleksei Borzetsov
- * date         28.03.2026
+ * date         31.03.2026
  */
 
-import HomeWork9.HomeWork9;
+import HomeWork10.HomeWork10;
 
 public class Main {
     public static void main(String[] args){
-        HomeWork9 hw9 = new HomeWork9();
-        hw9.run();
+        HomeWork10 hw10 = new HomeWork10();
+        hw10.run();
     }
 }
