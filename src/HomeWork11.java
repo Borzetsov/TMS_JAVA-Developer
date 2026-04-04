df5117f1cf0de116f49ee2f5585ee2cc9a47cf77
Alex <borzetsov@gmail.com>
2026-04-04 10:14:24 +0700

    HomeWork11 Initial commit

---
+++ src/HomeWork11.java
@@ -0,0 +1,16 @@
+/**
+ * Classname    HomeWork11
+ * @version     0.01
+ * @author      Aleksei Borzetsov
+ * date         04.04.2026
+ */
+
+public class HomeWork11 {
+
+    public void run() {
+
+        /*Шапка отчета*/
+        System.out.println("; Алексей Борзецов | Домашняя работа №11 | Выдано 01.04.2026 ;");
+        System.out.println();
+    }
+}
