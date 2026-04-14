f2fe3fc454a5230cbf3bb12f39d843f9427b8f84
Aleksei Borzetsov <borzetsov@gmail.com>
2026-04-14 20:34:25 +0700

    HomeWork14 Task1 + Task*

--- src/HomeWork14/HomeWork14.java
+++ src/HomeWork14/HomeWork14.java
@@ -8,6 +8,7 @@
 package HomeWork14;
 
 import java.io.*;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,9 +20,8 @@ public class HomeWork14 {
         System.out.println();
 
         System.out.println("Задача 1");
-        System.out.println();
         /*Чтение файла*/
-        File filePath = new File("src/HomeWork14/Task1/");
+        File filePath = new File("src\\HomeWork14\\Task1\\");
         File inputFile = new File(filePath, "RnJ_Synopsis.txt");
         try (FileReader inputFileReader = new FileReader(inputFile)) {
             StringBuilder inputText = new StringBuilder();
@@ -58,5 +58,102 @@ public class HomeWork14 {
             System.out.println("Ошибка чтения файла");
             System.out.println(e.getMessage());
         }
+        System.out.println("Задача 1 выполнена");
+        System.out.println();
+
+        System.out.println("Задача *");
+        System.out.println("Введите '+', чтобы создать файл test.txt");
+        System.out.println("или");
+        System.out.println("Введите произвольный символ, чтобы пропустить шаг");
+        Scanner consoleScanner = new Scanner(System.in);
+        if (consoleScanner.nextLine().equals("+")) {
+            /*Создать файл*/
+            File testFilePath = new File("src\\Homework14\\TaskAsterisk\\");
+            File test = new File(testFilePath, "test.txt");
+            try (FileWriter testWriter = new FileWriter(test)) {
+                /*Заполнить тестовый файл*/
+                double d;
+                String radnomDocNumber;
+                StringBuilder validDocNumber;
+                for (int i = 0; i < 100; i++) {
+                    d = Math.random();
+                    if (d < 0.33) {
+                        radnomDocNumber = String.valueOf((int)(Math.random() * 1000000000));
+                        validDocNumber = new StringBuilder("docnum000000000");
+                        validDocNumber.replace(15 - radnomDocNumber.length(), 16, radnomDocNumber);
+                        testWriter.write(validDocNumber + "\r\n");
+                    }
+                    else if (d < 0.66) {
+                        radnomDocNumber = String.valueOf((int)(Math.random() * 10000000));
+                        validDocNumber = new StringBuilder("contract0000000");
+                        validDocNumber.replace(15 - radnomDocNumber.length(), 16, radnomDocNumber);
+                        testWriter.write(validDocNumber + "\r\n");
+                    }
+                    else {
+                        testWriter.write("invalid contract " + i + "\r\n");
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Ошибка записи в файл");
+                System.out.println(e.getMessage());
+            }
+            System.out.println("Тестовый файл создан");
+        }
+        System.out.println("Укажите путь к файлу");
+        File userFile = new File(consoleScanner.nextLine());
+        try (FileReader userFileReader = new FileReader(userFile)) {
+            StringBuilder userText = new StringBuilder();
+            /*Чтение пользовательского файла*/
+            int currentChar;
+            while((currentChar = userFileReader.read()) != -1) userText.append((char)currentChar);
+            /*Разделить по строкам*/
+            String[] splitUserText = userText.toString().split("\r\n");
+
+            StringBuilder validDocNumberSB = new StringBuilder();
+            StringBuilder invalidDocNumberSB = new StringBuilder();
+
+            Pattern validDocNumPattern = Pattern.compile("^docnum[a-zA-Z0-9]{9}|^contrac[a-zA-Z0-9]{7}");
+            Matcher validDocNumMatcher;
+            for (String currentString : splitUserText) {
+                /*Проверка каждой строки*/
+                validDocNumMatcher = validDocNumPattern.matcher(currentString);
+                if (validDocNumMatcher.find()) {
+                    validDocNumberSB.append(currentString);
+                    validDocNumberSB.append("\r\n");
+                }
+                else {
+                    invalidDocNumberSB.append(currentString);
+                    /*Поиск причины*/
+                    invalidDocNumberSB.append("\t\tПричины:");
+                    if (currentString.length() != 15) invalidDocNumberSB.append("\tошибка длины");
+                    if (!(currentString.startsWith("docnum") || currentString.startsWith("contrac")))
+                        invalidDocNumberSB.append("\tошибка названия");
+                    else invalidDocNumberSB.append("\tсодержит запрещенные символы");
+                    invalidDocNumberSB.append("\r\n");
+                }
+            }
+            /*Сохранить файлы отчетов в ту же директорию*/
+            String userFilePath = userFile.getParent();
+            File validDocNumFile = new File(userFilePath, "validDocumentNumbers.txt");
+            File invalidDocNumFile = new File(userFilePath, "invalidDocumentNumbers.txt");
+            /*Записать валидные номера документов*/
+            try (FileWriter validDocNumWriter = new FileWriter(validDocNumFile)) {
+                validDocNumWriter.write(validDocNumberSB.toString());
+            } catch (IOException e) {
+                System.out.println("Ошибка создания файла");
+                System.out.println(e.getMessage());
+            }
+            /*Записать невалидные номера документов*/
+            try (FileWriter invalidDocNumWriter = new FileWriter(invalidDocNumFile)) {
+                invalidDocNumWriter.write(invalidDocNumberSB.toString());
+            } catch (IOException e) {
+                System.out.println("Ошибка создания файла");
+                System.out.println(e.getMessage());
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения файла");
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Задача * выполнена");
     }
 }
