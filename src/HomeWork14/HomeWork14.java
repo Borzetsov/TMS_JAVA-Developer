201197a46c54bfbd72151b2616bcd9f1f4650b1d
Alex <borzetsov@gmail.com>
2026-04-14 16:12:26 +0700

    HomeWork14 Task1

--- src/HomeWork14/HomeWork14.java
+++ src/HomeWork14/HomeWork14.java
@@ -8,6 +8,8 @@
 package HomeWork14;
 
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HomeWork14 {
 
@@ -22,14 +24,36 @@ public class HomeWork14 {
         File filePath = new File("src/HomeWork14/Task1/");
         File inputFile = new File(filePath, "RnJ_Synopsis.txt");
         try (FileReader inputFileReader = new FileReader(inputFile)) {
-            String theLongestWord = "";
             StringBuilder inputText = new StringBuilder();
             /*Составить текст*/
             int currentChar;
             while((currentChar = inputFileReader.read()) != -1) {
                 inputText.append((char)currentChar);
             }
-            System.out.println(inputText);
+            /*Вычленить слова*/
+            Pattern wordPattern = Pattern.compile("\\b[\\w]+\\b");  //Любое слово
+            Matcher wordMatcher = wordPattern.matcher(inputText);
+            StringBuilder cleanText = new StringBuilder();
+            while (wordMatcher.find()) {
+                cleanText.append(inputText.subSequence(wordMatcher.start(), wordMatcher.end()));
+                cleanText.append(" ");  //Разделитель
+            }
+            /*Разделить текст на слова*/
+            String[] cplitString = cleanText.toString().split(" ");
+            /*Найти самое длинное слово*/
+            String theLongestWord = "";
+            for (String currentString : cplitString) {
+                if (currentString.length() > theLongestWord.length()) {
+                    theLongestWord = currentString;
+                }
+            }
+            File outputFile = new File(filePath, "RnJ_TheLongestWord.txt");
+            try (FileWriter outputFileWriter = new FileWriter(outputFile)) {
+                outputFileWriter.write("The longest word of the text is: " + theLongestWord);
+            } catch (IOException e) {
+                System.out.println("Ошибка записи в файл");
+                System.out.println(e.getMessage());
+            }
         } catch (IOException e) {
             System.out.println("Ошибка чтения файла");
             System.out.println(e.getMessage());
