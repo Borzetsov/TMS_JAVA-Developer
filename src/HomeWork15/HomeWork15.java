7224cc776d26a05d3d965cddad6dc96ce569e52f
Alex <borzetsov@gmail.com>
2026-04-17 11:17:49 +0700

    HomeWork15 Initial commit

---
+++ src/HomeWork15/HomeWork15.java
@@ -0,0 +1,17 @@
+/**
+ * Classname    HomeWork15
+ * @version     0.01
+ * @author      Aleksei Borzetsov
+ * date         17.04.2026
+ */
+ 
+package HomeWork15;
+
+public class HomeWork15 {
+
+    public void run() {
+        /*Шапка отчета*/
+        System.out.println("; Алексей Борзецов | Домашняя работа №15 | Выдано 15.04.2026 ;");
+        System.out.println();
+    }
+}
