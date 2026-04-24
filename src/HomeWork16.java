160bfeff93a2d8b972f1183053da16cbefd66e95
Alex <borzetsov@gmail.com>
2026-04-24 13:05:33 +0700

    HomeWork16 Initial commit

---
+++ src/HomeWork16.java
@@ -0,0 +1,15 @@
+/**
+ * Classname    HomeWork16
+ * @version     0.01
+ * @author      Aleksei Borzetsov
+ * date         24.04.2026
+ */
+
+public class HomeWork16 {
+
+    public void run() {
+        /*Шапка отчета*/
+        System.out.println("; Алексей Борзецов | Домашняя работа №16 | Выдано 22.04.2026 ;");
+        System.out.println();
+    }
+}
