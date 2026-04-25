c86fbb1d8d0944c4abb146763395376fd061033f
Alex <borzetsov@gmail.com>
2026-04-25 14:25:45 +0700

    Account +save/load

--- src/CourseProjectJavaCore/CourseProjectJavaCore.java
+++ src/CourseProjectJavaCore/CourseProjectJavaCore.java
@@ -7,9 +7,68 @@
  
 package CourseProjectJavaCore;
 
+import CourseProjectJavaCore.model.Account;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CourseProjectJavaCore {
 
     public void run() {
         System.out.println("Курсовой проект №1 Java Core");
+        System.out.println();
+
+        System.out.println("Создание списка счетов");
+        ArrayList<Account> accounts = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            accounts.add(new Account());
+        }
+        for (Account currentAccount : accounts) System.out.println(currentAccount);
+        System.out.println();
+
+        System.out.println("Пополнение баланса");
+        for (Account currentAccount : accounts) {
+            currentAccount.income((long)(Math.random() * 1000000L));
+        }
+        for (Account currentAccount : accounts) System.out.println(currentAccount);
+        System.out.println();
+
+        System.out.println("Запись в файл");
+        File accountFilePath = new File("src\\CourseProjectJavaCore\\dataChannels\\");
+        File accountFile = new File(accountFilePath, "accounts.txt");
+
+        try (FileWriter accountWriter = new FileWriter(accountFile)) {
+            for (Account currentAccount : accounts) accountWriter.write(currentAccount.toString() + "\r\n");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Чтение из файла");
+        StringBuilder readAccountFile = new StringBuilder();
+        try (FileReader accountReader = new FileReader(accountFile)) {
+            int currentChar;
+            while((currentChar = accountReader.read()) != -1) {
+                readAccountFile.append((char)currentChar);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(readAccountFile);
+
+        System.out.println("Проверка регулярного выражения");
+        ArrayList<Account> newAccounts = new ArrayList<>();
+        Pattern accountPattern = Pattern.compile(Account.ACCOUNT_REGEXP);
+        Matcher accountMatcher = accountPattern.matcher(readAccountFile);
+        while (accountMatcher.find()) {
+            newAccounts.add(new Account(accountMatcher.group()));
+        }
+        System.out.println("Вывод прочитанных счетов");
+        for (Account currentAccount : newAccounts) System.out.println(currentAccount);
+        System.out.println();
     }
 }
--- src/CourseProjectJavaCore/model/Account.java
+++ src/CourseProjectJavaCore/model/Account.java
@@ -11,4 +11,68 @@ package CourseProjectJavaCore.model;
  * Класс счета
  */
 public class Account {
+
+    private static final long DEFAULT_INITIAL_BALANCE = 0L;
+
+    public static final String ACCOUNT_REGEXP = "Account: [0-9]{5}-[0-9]{5} Balance: [0-9]+\\b";
+
+    private final String number;
+    private long balance;
+
+    /**
+     * Создать новый счет
+     * Номер вида ххххх-ххххх
+     * генерируется автоматически
+     */
+    public Account() {
+        String randomNumber = String.valueOf((int)(Math.random() * 89999));
+        StringBuilder accountNumberTemplate = new StringBuilder("00000-00000");
+        accountNumberTemplate.replace(5 - randomNumber.length(), 5, randomNumber);
+        randomNumber = String.valueOf((int)(Math.random() * 89999));
+        accountNumberTemplate.replace(11 - randomNumber.length(), 11, randomNumber);
+        this.number = accountNumberTemplate.toString();
+        this.balance = DEFAULT_INITIAL_BALANCE;
+    }
+
+    public Account(String record) {
+        String[] recordComponents = record.split(" ");
+        this.number = recordComponents[1];
+        this.balance = Integer.parseInt(recordComponents[3]);
+    }
+
+    /**
+     * Пополнить баланс
+     * @param value -- величина пополнения
+     * @return -- статус операции
+     */
+    public BalanceChangeStatus income(long value) {
+        if (value <= 0) return BalanceChangeStatus.BALANCE_CHANGE_STATUS_ILLEGAL_VALUE;
+        balance += value;
+        return BalanceChangeStatus.BALANCE_CHANGE_STATUS_OK;
+    }
+
+    /**
+     * Уменьшить баланс
+     * @param value -- величина списания
+     * @return -- статус операции
+     */
+    public BalanceChangeStatus expense(long value) {
+        if (value <= 0) return BalanceChangeStatus.BALANCE_CHANGE_STATUS_ILLEGAL_VALUE;
+        if ((this.balance - value) < 0) return BalanceChangeStatus.BALANCE_CHANGE_STATUS_INSUFFICIENT_FUNDS;
+        this.balance -= value;
+        return BalanceChangeStatus.BALANCE_CHANGE_STATUS_OK;
+    }
+
+    @Override
+    public String toString() {
+        return "Account: " + this.number + " Balance: " + this.balance;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    public long getBalance() {
+        return this.balance;
+    }
 }
