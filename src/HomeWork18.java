244fd26c988c11c8284f3cf7d7b338b7218fd60f
Aleksei Borzetsov <borzetsov@gmail.com>
2026-05-09 17:19:55 +0700

    HomeWork18 Initial commit

---
+++ src/HomeWork18.java
@@ -0,0 +1,16 @@
+/**
+ * Classname    HomeWork18
+ * @version     0.01
+ * @author      Aleksei Borzetsov
+ * date         09.05.2026
+ */
+
+public class HomeWork18 {
+
+    public void run() {
+        /*Шапка отчета*/
+        System.out.println("; Алексей Борзецов | Домашняя работа №18 | Выдано 29.04.2026 ;");
+        System.out.println();
+    }
+    
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 /**
  * Classname    Main
- * @version     0.16
+ * @version     0.18
  * @author      Aleksei Borzetsov
- * date         24.04.2026
+ * date         09.05.2026
  */
 
 public class Main {
     public static void main(String[] args){
-        HomeWork16 hw16 = new HomeWork16();
-        hw16.run();
+        HomeWork18 hw18 = new HomeWork18();
+        hw18.run();
     }
 }
