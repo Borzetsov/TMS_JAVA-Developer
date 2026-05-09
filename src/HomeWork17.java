42db61a283c876dd095bb37cc5c619eacd76181c
Aleksei Borzetsov <borzetsov@gmail.com>
2026-05-09 17:11:04 +0700

    HomeWork17 Initial commit

---
+++ src/HomeWork17.java
@@ -0,0 +1,15 @@
+/**
+ * Classname    HomeWork17
+ * @version     0.01
+ * @author      Aleksei Borzetsov
+ * date         09.05.2026
+ */
+
+public class HomeWork17 {
+
+    public void run() {
+        /*Шапка отчета*/
+        System.out.println("; Алексей Борзецов | Домашняя работа №17 | Выдано 27.04.2026 ;");
+        System.out.println();
+    }
+}
